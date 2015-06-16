fee7e6809e1eb158e50c78de1310615e9bcbce16
brauliomeyer <braulio_meijer@yahoo.com>
2015-06-16 09:43:28 +0200

    Test
    Test

---
+++ app.js
@@ -0,0 +1,13 @@
+(function () {
+    var com = {};
+    com.mydomain = {};
+    com.mydomain.AProgram = {};
+    
+    com.mydomain.AProgram.utility = function() {
+        console.log("Programmer A utility function");
+    }
+
+    com.mydomain.AProgram.utility();
+}()); 
+
+
